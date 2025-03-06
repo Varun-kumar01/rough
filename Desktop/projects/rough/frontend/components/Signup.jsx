af5134aedcf6280b5ed9bf22ae246dd942e84add
varun <varunkumargangipelli@gmail.com>
2025-03-06 11:54:47 +0530

    added all the files

---
+++ Desktop/projects/rough/frontend/components/Signup.jsx
@@ -0,0 +1,5 @@
+export function Signup(){
+    return (
+        <button>click me</button>
+    )
+}
---
+++ Desktop/projects/rough/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+  import React from 'react';
+  import './App.css'
+  
+
+
+  function App() {
+    return (<button>click me</button>)
+  }
+
+  export default App
